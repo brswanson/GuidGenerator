7d4392af8f88365e03e7489e3338755cce03175f
Ben R. Swanson <brswanson@users.noreply.github.com>
2019-05-09 11:31:52 -0500

    Added a simple logger.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace GuidGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Log(typeof(Program).Namespace, Guid.NewGuid().ToString());
+                Thread.Sleep(5000);
+            }
+        }
+
+        private static void Log(string caller, string msg)
+        {
+            try
+            {
+                var output = $"{DateTime.UtcNow:s}|INFO|{msg}{Environment.NewLine}";
+
+                Console.Write(output);
+                File.AppendAllTextAsync($"{caller}.log", output);
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+        }
+    }
+}
